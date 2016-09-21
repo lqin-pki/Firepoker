511928e07b4394b13f2e0e100d6896b51ba9f174
lqin-pki <leon.qin@perkinelmer.com>
2016-09-21 12:21:24 +0800

    Change to our own firebase URL.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,7 +15,7 @@ angular.module('firePokerApp')
   .controller('MainCtrl', function ($rootScope, $scope, $cookieStore, $location, $routeParams, $timeout, angularFire) {
 
     // Firebase URL
-    var URL = 'https://pzfqrq7kjy.firebaseio.com';
+    var URL = 'https://poker-21761.firebaseio.com';
 
     // Initialize Firebase
     /*global Firebase*/
